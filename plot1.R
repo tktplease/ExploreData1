8cefd1941bb573067881d68d026d32d171103c39
tktplease <jeffforbeck@yahoo.com>
2015-08-09 15:49:05 -0700

    Revert "Project 1"
    This reverts commit 4b631fe87c0d1c9c8175f343e74adbdbbd7836fa.

--- plot1.R
+++
@@ -1,7 +0,0 @@
-subset_power <- house_power[house_power$Pdate == as.Date("2007-02-01") | house_power$Pdate ==  as.Date("2007-02-02"),]
-
-hist(subset_power$Global_active_power,col="Orange",xlab="Global Active Power (kilowatts)",main = "Global Active Power")
-
-dev.copy(png,"plot1.png")
-
-dev.off()
--- plot2.R
+++
@@ -1,7 +0,0 @@
-subset_power <- house_power[house_power$Pdate == as.Date("2007-02-01") | house_power$Pdate ==  as.Date("2007-02-02"),]
-
-plot(subset_power$Ptime,subset_power$Global_active_power,ylab="Global Active Power (kilowatts)",main = "Plot 2",type="l")
-
-dev.copy(png,"plot2.png")
-
-dev.off()
